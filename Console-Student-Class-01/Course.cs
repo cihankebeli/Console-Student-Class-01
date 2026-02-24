c7cfbc871204fee4f322bdee7990034e79001798
Ozgur <ozgurozvaris@hotmail.com>
2026-02-25 00:28:37 +0300

    initial commit

---
+++ Console-Student-Class-01/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Student_Class_01
+{
+    internal class Course
+    {
+        public int CourseID { get; set; }
+
+        public string CourseName { get; set; }
+
+        public string CourseCode { get; set; }
+
+        public int CourseCredit { get; set; }
+    }
+}
---
+++ Console-Student-Class-01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Console_Student_Class_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+            course.CourseID = 1;
+            course.CourseName = "Mathematics";
+            course.CourseCode = "MATH101";
+            course.CourseCredit = 3;
+
+            Course course1 = new Course();
+            course1.CourseID = 2;
+            course1.CourseName = "Physics";
+            course1.CourseCode = "PHYS101";
+            course1.CourseCredit = 4;
+
+            Course course2 = new Course();
+            course2.CourseID = 3;
+            course2.CourseName = "Chemistry";
+            course2.CourseCode = "CHEM101";
+            course2.CourseCredit = 3;
+
+            List<Course> courseList = new List<Course>();
+            List<Student> studentList = new List<Student>();
+
+            Console.WriteLine("Course count: " + (courseList.Count() > 0 ? courseList.Count() : "0"));
+            // Console.WriteLine($"Course count: {courseList?.Count ?? 0}");
+            Console.ReadLine();
+        }
+    }
+}
