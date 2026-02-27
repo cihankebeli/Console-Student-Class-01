e39f64bd3e8a39d4e51734a3cba2343f86958354
Ozgur <ozgurozvaris@hotmail.com>
2026-02-27 14:38:01 +0300

    update code

--- Console-Student-Class-01/Program.cs
+++ Console-Student-Class-01/Program.cs
@@ -2,6 +2,32 @@
 {
     internal class Program
     {
+
+        public static Student RegisterStudents()
+        {
+
+            Console.WriteLine("Enter student Id:");
+            int studentId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter student name:");
+            string studentName = Console.ReadLine();
+
+            Console.WriteLine("Enter student surname:");
+            string studentSurname = Console.ReadLine();
+
+            Console.WriteLine("Enter student email:");
+            string studentEmail = Console.ReadLine();
+
+            Student student = new Student();
+
+            student.StudentID = studentId;
+            student.StudentName = studentName;
+            student.StudentSurname = studentSurname;
+            student.StudentEmail = studentEmail;
+
+            return student;
+
+        }
         static void Main(string[] args)
         {
             Course course = new Course();
@@ -25,8 +51,63 @@
             List<Course> courseList = new List<Course>();
             List<Student> studentList = new List<Student>();
 
+            courseList.Add(course);
+            courseList.Add(course1);
+            courseList.Add(course2);
+
             Console.WriteLine("Course count: " + (courseList.Count() > 0 ? courseList.Count() : "0"));
             // Console.WriteLine($"Course count: {courseList?.Count ?? 0}");
+
+            //foreach (var item in courseList)
+            //{
+            //    Console.WriteLine($"Course ID = {item.CourseID} Course Name = {item.CourseName} Course Code = {item.CourseCode} Course Credit = {item.CourseCredit}");
+            //}
+
+            //Student student = RegisterStudents();
+
+            //studentList.Add(student);
+
+            //student = RegisterStudents();
+
+            //studentList.Add(student);
+
+            //foreach (var item in studentList)
+            //{
+            //    Console.WriteLine($"Student ID = {item.StudentID} Student Name = {item.StudentName} Student Surname = {item.StudentSurname} Student Email = {item.StudentEmail}");
+            //}
+
+            Student student = RegisterStudents();
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("Student Registration System");
+                Console.WriteLine("1 - Do you want to add a new student");
+                Console.WriteLine("2 - Lists students");
+                Console.WriteLine("3 - Exit");
+                string answer = Console.ReadLine();
+                if (answer == "1")
+                {
+                    student = RegisterStudents();
+                    studentList.Add(student);
+                } else if (answer == "2")
+                {
+                    Console.WriteLine("Student List");
+                    foreach (var item in studentList)
+                    {
+                        Console.WriteLine($"Student ID = {item.StudentID} Student Name = {item.StudentName} Student Surname = {item.StudentSurname} Student Email = {item.StudentEmail}");
+                    }
+                }
+                else if (answer == "3")
+                {
+                    exit = true;
+                }
+                else                 {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+
             Console.ReadLine();
         }
     }
